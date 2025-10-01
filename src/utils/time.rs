34d308650c03cd797dd35453f79510befd40a9fb
Khenziii <khenzii.was.taken@gmail.com>
2025-10-01 18:52:15 +0200

    feat: write tests for time helpers

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -15,3 +15,37 @@ pub fn str_to_system_time(formatted_string: String) -> SystemTime {
         .expect("Failed to convert passed timestamp to integer! Is the string surely valid?");
     UNIX_EPOCH + Duration::from_secs(timestamp)
 }
+
+#[cfg(test)]
+mod tests {
+    mod system_time_to_str {
+        use super::super::*;
+
+        #[test]
+        fn converts_correctly() {
+            let timestamp = UNIX_EPOCH + Duration::from_secs(5);
+            let timestamp_str = system_time_to_str(timestamp);
+
+            assert_eq!(timestamp_str, "5");
+        }
+
+        #[test]
+        #[should_panic]
+        fn value_range_valid() {
+            let timestamp = UNIX_EPOCH - Duration::from_secs(5);
+            system_time_to_str(timestamp);
+        }
+    }
+
+    mod str_to_system_time {
+        use super::super::*;
+
+        #[test]
+        fn converts_correctly() {
+            let timestamp = UNIX_EPOCH + Duration::from_secs(5);
+            let timestamp_str = String::from("5");
+
+            assert_eq!(timestamp, str_to_system_time(timestamp_str));
+        }
+    }
+}
