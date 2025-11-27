09c00199d57915392300fbda07062cfdd4c7b8f1
Khenziii <khenzii.was.taken@gmail.com>
2025-11-27 16:39:02 +0100

    feat(scripts): make `run` script use `dotenv`
    It handles longer secrets better

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -e "$HOME/asmda-secrets.env" ]; then
-    env $(grep -v '^#' $HOME/asmda-secrets.env | xargs) asmda-unwrapped "$@"
+    dotenv -e $HOME/asmda-secrets.env asmda-unwrapped "$@"
 else
     echo "\`$HOME/asmda-secrets.env\` doesn't exist! Please either create it and specify the configuration there (for a quickstart copy one from the documentation), or use \`asmda-unwrapped\` to manually specify the environment variables, without automatically loading them from this file."
 fi
